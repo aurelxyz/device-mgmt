f5ab72f6cec355610e2497eb63d3fd176fdfde9f
Aurélien Renou <aurelien.renou@gmail.com>
2025-07-29 22:01:07 +0200

    Make API_KEYS configurable via environment variables

--- backend/src/utils/checkApiKey.ts
+++ backend/src/utils/checkApiKey.ts
@@ -1,14 +1,20 @@
 import { type Request, type Response, type NextFunction } from 'express';
 import { httpError } from './http-error.ts';
+import { trim } from 'zod';
 
-const API_KEY = '1234'; // TODO
+const envKeys = process.env.API_KEYS;
+if (!envKeys) {
+  throw new Error('❌ Environment variable API_KEYS is not defined.')
+}
+
+const acceptedKeys = envKeys.split(',').map(key => key.trim());
 
 // Middleware to check the API key
 export const checkApiKey = (req: Request, res: Response, next: NextFunction) => {
-  const apiKey = req.headers['x-api-key'];
+  const apiKey = req.header('x-api-key');
 
   // Check if the API key is provided and matches the expected value
-  if (!apiKey || apiKey !== API_KEY) {
+  if (!apiKey || !acceptedKeys.includes(apiKey)) {
     const err = httpError(401, "Unauthorized");
     return next(err);
   }
